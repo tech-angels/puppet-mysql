10f8fd49e9390520952d3f7cfa97017d02df9eb5
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-05-16 12:04:33 +0200

    Fix master and slave classes

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,23 +1,3 @@
 class mysql::master inherits mysql::server {
-
-  Mysql::Config['log-bin'] {
-    ensure => 'present',
-    value  => 'mysql-bin',
-  }
-
-  Mysql::Config['server-id'] {
-    ensure => 'present',
-    value  => ${mysql_serverid},
-  }
-
-  Mysql::Config['expire_logs_days'] {
-    ensure => 'present',
-    value  => '7',
-  }
-
-  Mysql::Config['max_binlog_size'] [
-    ensure => 'present',
-    value  => '100M',
-  }
-
+  include mysql::config::replication::master
 }
