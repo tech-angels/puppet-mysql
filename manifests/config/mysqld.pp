c1114a2159d04a27021b6f3e94f125c1e987fefd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-05-18 10:47:26 +0200

    Fix log-error and log-slow-queries in mysql::config::mysqld

--- manifests/config/mysqld.pp
+++ manifests/config/mysqld.pp
@@ -6,6 +6,10 @@ class mysql::config::mysqld {
     'log-warnings':         value => '1';
     'datadir':              value => "${mysql::params::data_dir}";
     'log-error':            value => $::operatingsystem ? {
+      /RedHat|Fedora|CentOS/ => '/var/log/mysqld.log',
+      default                => '/var/log/mysql.err',
+      };
+    'log-slow-queries':     value => $::operatingsystem ? {
       /RedHat|Fedora|CentOS/ => '/var/log/mysql-slow-queries.log',
       default                => '/var/log/mysql/mysql-slow.log',
       };
