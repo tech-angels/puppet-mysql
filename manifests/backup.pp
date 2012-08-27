d014e199aa42d3cf21505e2a84d05c43cab9b6bb
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-08-27 09:53:59 +0200

    mysql::backup allows now to pass options to mysqldump

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -8,8 +8,12 @@ mysqldump --all-databases. Backups will be stored in /var/backups/mysql.
 
 Attributes:
 - $mysqldump_retention: defines if backup rotate on a weekly, monthly or yearly
-  basis. Accepted values: "week", "month", "year". Defaults to "week".
-$subversion_backupdir = "/var/backups/subversion"
+  basis. Accepted values: 'week', 'month', 'year'. Defaults to 'week'.
+- $mysql_backupdir: defines backup location
+  Default value: '/var/backups/mysql'
+- $mysqldump_options: defines options passed to mysqldump command.
+  Please refer to the manpage.
+  Default value: '--all-database --extended-insert'
 
 */
 class mysql::backup {
@@ -17,33 +21,39 @@ class mysql::backup {
   include mysql::common
   include mysql::params
 
-  if $mysqldump_retention {} else { $mysqldump_retention = "week" }
+  if !$mysqldump_retention {
+    $mysqldump_retention = 'week'
+  }
+  if !$mysqldump_options {
+    $mysqldump_options = '--all-database --extended-insert'
+  }
+
 
   $data_dir = $mysql::params::data_dir
   $backup_dir = $mysql::params::backup_dir
 
-  file { "${backup_dir}":
+  file {$backup_dir:
     ensure  => directory,
-    owner   => "root",
-    group   => "mysql-admin",
-    mode    => 750,
-    require => Group["mysql-admin"]
+    owner   => 'root',
+    group   => 'mysql-admin',
+    mode    => '0750',
+    require => Group['mysql-admin']
   }
 
-  file { "/usr/local/bin/mysql-backup.sh":
+  file {'/usr/local/bin/mysql-backup.sh':
     ensure  => present,
-    content => template("mysql/mysql-backup.sh.erb"),
-    owner   => "root",
-    group   => "root",
-    mode    => 555,
+    content => template('mysql/mysql-backup.sh.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
   }
 
-  cron { "mysql-backup":
+  cron {'mysql-backup':
     command => "/usr/local/bin/mysql-backup.sh ${mysqldump_retention}",
-    user    => "root",
+    user    => 'root',
     hour    => 2,
     minute  => 0,
-    require => [File["${backup_dir}"], File["/usr/local/bin/mysql-backup.sh"]],
+    require => [File[$backup_dir], File['/usr/local/bin/mysql-backup.sh']],
   }
 
 }
