9b2b3683731c5a611a8652e65bb9f371347b222e
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2013-08-06 15:41:35 +0000

    Fix execution for ruby 1.9

--- lib/puppet/provider/mysql_database/mysql.rb
+++ lib/puppet/provider/mysql_database/mysql.rb
@@ -1,67 +1,67 @@
 # -*- tab-width: 4; ruby-indent-level: 4; indent-tabs-mode: t -*-
 Puppet::Type.type(:mysql_database).provide :mysql, :parent => Puppet::Provider::Package do
-	desc "Provide MySQL interactions via /usr/bin/mysql"
+  desc "Provide MySQL interactions via /usr/bin/mysql"
 
-	# this is a bit of a hack.
-	# Since puppet evaluates what provider to use at start time rather than run time
-	# we can't specify that commands will exist. Instead we call manually.
-	# I would make these call execute directly, but execpipe needs the path
-	def self.mysqladmin
-		'/usr/bin/mysqladmin'
-	end
-	def self.mysql
-		'/usr/bin/mysql'
-	end
-	def mysqladmin
-		self.class.mysqladmin
-	end
-	def mysql
-		self.class.mysql
-	end
+  # this is a bit of a hack.
+  # Since puppet evaluates what provider to use at start time rather than run time
+  # we can't specify that commands will exist. Instead we call manually.
+  # I would make these call execute directly, but execpipe needs the path
+  def self.mysqladmin
+    '/usr/bin/mysqladmin'
+  end
+  def self.mysql
+    '/usr/bin/mysql'
+  end
+  def mysqladmin
+    self.class.mysqladmin
+  end
+  def mysql
+    self.class.mysql
+  end
 
-	# retrieve the current set of mysql users
-	def self.instances
-		dbs = []
+  # retrieve the current set of mysql users
+  def self.instances
+    dbs = []
 
-		cmd = "#{mysql} mysql -NBe 'show databases'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				dbs << new( { :ensure => :present, :name => line.chomp } )
-			end
-		end
-		return dbs
-	end
+    cmd = "#{mysql} mysql -NBe 'show databases'"
+    execpipe(cmd) do |process|
+      process.each_line do |line|
+        dbs << new( { :ensure => :present, :name => line.chomp } )
+      end
+    end
+    return dbs
+  end
 
-	def query
-		result = {
-			:name => @resource[:name],
-			:ensure => :absent
-		}
+  def query
+    result = {
+      :name => @resource[:name],
+      :ensure => :absent
+    }
 
-		cmd = "#{mysql} mysql -NBe 'show databases'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				if line.chomp.eql?(@resource[:name])
-					result[:ensure] = :present
-				end
-			end
-		end
-		result
-	end
+    cmd = "#{mysql} mysql -NBe 'show databases'"
+    execpipe(cmd) do |process|
+      process.each_line do |line|
+        if line.chomp.eql?(@resource[:name])
+          result[:ensure] = :present
+        end
+      end
+    end
+    result
+  end
 
-	def create
-		execute([mysqladmin, "create", @resource[:name]], {custom_environment: "HOME=/root"})
-	end
-	def destroy
-		execute([mysqladmin, "-f", "drop", @resource[:name]], {custom_environment: "HOME=/root"})
-	end
+  def create
+    execute([mysqladmin, "create", @resource[:name]], {custom_environment: "HOME=/root"})
+  end
+  def destroy
+    execute([mysqladmin, "-f", "drop", @resource[:name]], {custom_environment: "HOME=/root"})
+  end
 
-	def exists?
-		if execute([mysql, "mysql", "-NBe", "show databases"], {custom_environment: "HOME=/root"}).match(/^#{@resource[:name]}$/)
-			true
-		else
-			false
-		end
-	end
+  def exists?
+    if execute([mysql, "mysql", "-NBe", "show databases"], {custom_environment: "HOME=/root"}).match(/^#{@resource[:name]}$/)
+      true
+    else
+      false
+    end
+  end
 end
 
--- lib/puppet/provider/mysql_user/mysql.rb
+++ lib/puppet/provider/mysql_user/mysql.rb
@@ -2,92 +2,92 @@
 require 'puppet/provider/package.rb'
 Puppet::Type.type(:mysql_user).provide :mysql, :parent => Puppet::Provider::Package  do
 
-	desc "Use mysql as database."
-	# this is a bit of a hack.
-	# Since puppet evaluates what provider to use at start time rather than run time
-	# we can't specify that commands will exist. Instead we call manually.
-	# I would make these call execute directly, but execpipe needs the path
-	def self.mysqladmin
-		'/usr/bin/mysqladmin'
-	end
-	def self.mysql
-		'/usr/bin/mysql'
-	end
-	def mysqladmin
-		self.class.mysqladmin
-	end
-	def mysql
-		self.class.mysql
-	end
+  desc "Use mysql as database."
+  # this is a bit of a hack.
+  # Since puppet evaluates what provider to use at start time rather than run time
+  # we can't specify that commands will exist. Instead we call manually.
+  # I would make these call execute directly, but execpipe needs the path
+  def self.mysqladmin
+    '/usr/bin/mysqladmin'
+  end
+  def self.mysql
+    '/usr/bin/mysql'
+  end
+  def mysqladmin
+    self.class.mysqladmin
+  end
+  def mysql
+    self.class.mysql
+  end
 
-	# retrieve the current set of mysql users
-	def self.instances
-		users = []
+  # retrieve the current set of mysql users
+  def self.instances
+    users = []
 
-		cmd = "#{mysql} mysql -NBe 'select concat(user, \"@\", host), password from user'"
-		execpipe(cmd) do |process|
-			process.each do |line|
-				users << new( query_line_to_hash(line) )
-			end
-		end
-		return users
-	end
+    cmd = "#{mysql} mysql -NBe 'select concat(user, \"@\", host), password from user'"
+    execpipe(cmd) do |process|
+      process.each_line do |line|
+        users << new( query_line_to_hash(line) )
+      end
+    end
+    return users
+  end
 
-	def self.query_line_to_hash(line)
-		fields = line.chomp.split(/\t/)
-		{
-			:name => fields[0],
-			:password_hash => fields[1],
-			:ensure => :present
-		}
-	end
+  def self.query_line_to_hash(line)
+    fields = line.chomp.split(/\t/)
+    {
+      :name => fields[0],
+      :password_hash => fields[1],
+      :ensure => :present
+    }
+  end
 
-	def mysql_flush 
-		execute([mysqladmin, "flush-privileges"], {custom_environment: "HOME=/root"})
-	end
+  def mysql_flush
+    execute([mysqladmin, "flush-privileges"], {custom_environment: "HOME=/root"})
+  end
 
-	def query
-		result = {}
+  def query
+    result = {}
 
-		cmd = "#{mysql} -NBe 'select concat(user, \"@\", host), password from user where concat(user, \"@\", host) = \"%s\"'" % @resource[:name]
-		execpipe(cmd) do |process|
-			process.each do |line|
-				unless result.empty?
-					raise Puppet::Error,
-						"Got multiple results for user '%s'" % @resource[:name]
-				end
-				result = query_line_to_hash(line)
-			end
-		end
-		result
-	end
+    cmd = "#{mysql} -NBe 'select concat(user, \"@\", host), password from user where concat(user, \"@\", host) = \"%s\"'" % @resource[:name]
+    execpipe(cmd) do |process|
+      process.each_line do |line|
+        unless result.empty?
+          raise Puppet::Error,
+            "Got multiple results for user '%s'" % @resource[:name]
+        end
+        result = query_line_to_hash(line)
+      end
+    end
+    result
+  end
 
-	def create
-		# There is a longstanding MySQL bug where a user that does not appear to exist (previously deleted, etc) still cannot be created.
-		# http://bugs.mysql.com/bug.php?id=28331
-		# A workaround is to unconditionally drop the user and ignore the return value
-		execute([mysql, "mysql", "-e", "drop user '%s'" % [ @resource[:name].sub("@", "'@'") ]],
-				{:failonfail => false, :custom_environment => "HOME=/root"})
-		execute([mysql, "mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.should(:password_hash) ]], {custom_environment: "HOME=/root"})
-		mysql_flush
-	end
+  def create
+    # There is a longstanding MySQL bug where a user that does not appear to exist (previously deleted, etc) still cannot be created.
+    # http://bugs.mysql.com/bug.php?id=28331
+    # A workaround is to unconditionally drop the user and ignore the return value
+    execute([mysql, "mysql", "-e", "drop user '%s'" % [ @resource[:name].sub("@", "'@'") ]],
+        {:failonfail => false, :custom_environment => "HOME=/root"})
+    execute([mysql, "mysql", "-e", "create user '%s' identified by PASSWORD '%s'" % [ @resource[:name].sub("@", "'@'"), @resource.should(:password_hash) ]], {custom_environment: "HOME=/root"})
+    mysql_flush
+  end
 
-	def destroy
-		execute([mysql, "mysql", "-e", "drop user '%s'" % @resource[:name].sub("@", "'@'")], {custom_environment: "HOME=/root"})
-		mysql_flush
-	end
+  def destroy
+    execute([mysql, "mysql", "-e", "drop user '%s'" % @resource[:name].sub("@", "'@'")], {custom_environment: "HOME=/root"})
+    mysql_flush
+  end
 
-	def exists?
-		not execute([mysql, "mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource[:name]], {custom_environment: "HOME=/root"}).empty?
-	end
+  def exists?
+    not execute([mysql, "mysql", "-NBe", "select '1' from user where CONCAT(user, '@', host) = '%s'" % @resource[:name]], {custom_environment: "HOME=/root"}).empty?
+  end
 
-	def password_hash
-		@property_hash[:password_hash]
-	end
+  def password_hash
+    @property_hash[:password_hash]
+  end
 
-	def password_hash=(string)
-		execute([mysql, "mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ]], {custom_environment: "HOME=/root"})
-		mysql_flush
-	end
+  def password_hash=(string)
+    execute([mysql, "mysql", "-e", "SET PASSWORD FOR '%s' = '%s'" % [ @resource[:name].sub("@", "'@'"), string ]], {custom_environment: "HOME=/root"})
+    mysql_flush
+  end
 end
 
